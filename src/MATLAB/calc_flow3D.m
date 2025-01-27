8dc45192279b029a4cdbb252ad532e7a3592ffe1
Rachel Lee <65371136+ScientistRachel@users.noreply.github.com>
2025-01-27 15:33:51 -0500

    Initial calc_flow3D function

---
+++ src/MATLAB/calc_flow3D.m
@@ -0,0 +1,291 @@
+function [vx,vy,vz,rel] = calc_flow3D(images ,xyzSig, tSig, wSig)
+% The calc_flow3D function calculates optical flow velocities for a single
+% z-stack of images. Surrounding z-stacks in time are necessary to perform
+% the calculations. To peform calculations on an entire timelapse, see
+% parse_flow.m
+%
+% Usage: [vx,vy,vz,rel] = calc_flow3D(images ,xyzSig, tSig, wSig)
+%
+% Inputs:
+% images = 4D matrix with dimensions N_X, N_Y, N_Z, N_T
+%           N_T should be odd as only the central timepoint will be analyzed.
+%           N_T must be greater than or equal to 3*tSig+1.
+% xyzSig = sigma value for smoothing in all spatial dimensions. Default 3.
+%           Larger values remove noise but remove spatial detail.
+% tSig   = sigma value for smoothing in the temporal dimension. Default 1.
+%           Larger values remove noise but remove temporal detail.
+% wSig   = sigma value for Lucas-Kanade neighborhood. Default is 4.
+%           Larger values include a larger neighboorhood in the
+%           Lucas-Kanade constraint and will smooth over small features.
+%
+% Outputs:
+% vx     = Velocity in the x direction, reported as pixels/frame
+% vy     = Velocity in the y direction, reported as pixels/frame
+% vz     = Velocity in the z direction, reported as pixels/frame
+% rel    = Reliability, the smallest eigenvalue of (A'wA)
+%           This is a measure of confidence in the linear algebra solution
+%           and can be used to mask the velocities for downstream analysis.
+%
+% Change Log:
+% 2025/01/27 Rachel M. Lee - Created function
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Process Inputs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Check that the images are 3D + time
+if length(size(images)) ~= 4
+    error('Error: Input image must be a 4D matrix with dimensions N_X, N_Y, N_Z, N_T')
+end
+
+% Implement default values if they are not specified
+if ~exist('xyzSig','var') || isempty(xyzSig)
+    disp('Using default xyzSig value of 3...')
+    xyzSig = 3;
+end
+if ~exist('tSig','var') || isempty(tSig)
+    tSig = 1;
+    disp('Using default tSig value of 1...')
+end
+if ~exist('wSig','var') || isempty(wSig)
+    wSig = 4;
+    disp('Using default wSig value of 4...')
+end
+
+% Check image size against tSig
+Nt =  size(images,4);
+if Nt < 3*tSig+1
+    error('Error: Input images will lead to edge effects. N_T must be >= 3*tSig+1')
+end
+if ~mod(Nt,2) % enforce odd number
+    error('Error: Input images must have an odd number of timepoints. Only the central time point is analyzed')
+end
+NtSlice = ceil(Nt/2);
+
+
+%% Set up filters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Common spatial terms
+x = -ceil(3*xyzSig):ceil(3*xyzSig);
+xySig2 = xyzSig/4;
+y = -ceil(3*xySig2):ceil(3*xySig2);
+fderiv = exp(-x.*x/2/xyzSig/xyzSig)/sqrt(2*pi)/xyzSig;
+fsmooth = exp(-y.*y/2/xySig2/xySig2)/sqrt(2*pi)/xySig2;
+gderiv = x/xyzSig/xyzSig;
+gsmooth = 1;
+
+%   Build x-gradient filter kernels (along first dimension)
+xFil1 = (fderiv.*gderiv)';
+yFil1 = (fsmooth.*gsmooth);
+zFil1 = permute(yFil1,[3 1 2]);
+%   Build y-gradient filter kernels (along second dimension)
+xFil2 = (fsmooth.*gsmooth)';
+yFil2 = (fderiv.*gderiv);
+zFil2 = permute(xFil2,[3 2 1]);
+%   Build z-gradient filter kernels (along third dimension)
+xFil3 = (fsmooth.*gsmooth)';
+yFil3 = (fsmooth.*gsmooth);
+zFil3 = permute(fderiv.*gderiv,[3 1 2]);
+
+%   Build t-gradient filter kernels (t = fourth dimension)
+t = -ceil(3*tSig):ceil(3*tSig);
+fx = exp(-x.*x/2/xyzSig/xyzSig)/sqrt(2*pi)/xyzSig;
+ft = exp(-t.*t/2/tSig/tSig)/sqrt(2*pi)/tSig;
+gx = 1;
+gt = t/tSig/tSig;
+xFil4 = (fx.*gx)';
+yFil4 = xFil4';
+zFil4 = permute(yFil4,[3 1 2]);
+tFil4 = permute(ft.*gt, [4, 1, 3, 2]);
+
+% Structure tensor -- Lucas Kanade neighborhood filter
+wRange = -ceil(3*wSig):ceil(3*wSig);
+gw = exp(-wRange.*wRange/2/wSig/wSig)/sqrt(2*pi)/wSig;
+xFil5 = (gw)';
+yFil5 = (gw);
+zFil5 = permute(gw,[3 1 2]);
+
+clear gderiv gsmooth gt gw gx ft fx fsmooth fderiv x y t gw wRange
+
+%% Spatial and Temporal Gradients %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Spatial gradients require at least N_T = 3 to avoid edge effets, while 
+% the temporal gradient requires N_T >= 3*tSig+1.
+% After gradients are calculated, only the central 3 slices are kept
+
+% dx
+dxI = imfilter(imfilter(imfilter(images, xFil1, 'replicate'), yFil1, 'replicate'), zFil1, 'replicate'); % Filtering to calculate the gradient
+dxI = dxI(:,:,:,NtSlice-1:NtSlice+1); % Only need the slice of interest plus one timepoint before and after for remaining calculations, simplify to save memory
+clear xFil1 yFil1 zFil1
+
+% dy
+dyI = imfilter(imfilter(imfilter(images, xFil2, 'replicate'), yFil2, 'replicate'), zFil2, 'replicate');
+dyI = dyI(:,:,:,NtSlice-1:NtSlice+1);
+clear xFil2 yFil2 zFil2
+
+% dz
+dzI = imfilter(imfilter(imfilter(images, xFil3, 'replicate'), yFil3, 'replicate'), zFil3, 'replicate');
+dzI = dzI(:,:,:,NtSlice-1:NtSlice+1);
+clear xFil3 yFil3 zFil3
+
+% dz
+dtI = imfilter(imfilter(imfilter(imfilter(images, xFil4, 'replicate'), yFil4, 'replicate'), zFil4, 'replicate'), tFil4, 'replicate');
+clear images % No longer needed for calculations; clear to save memory
+dtI = dtI(:,:,:,NtSlice-1:NtSlice+1);
+clear xFil4 yFil4 zFil4 tFil4
+
+%% Structure Tensor Inputs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The following calculations are for the individual elements of the
+% matrices required for the optical flow calculation, incorporating
+% Gaussian weighting into the Lucas-Kanade constraint.
+%
+% Once filtering is done, only the central timepoint is kept to save save
+% memory. Because all outputs of the above section are N_X x N_Y x N_Z x 3,
+% the central slice = 2.
+
+% Time componenents
+wdtx = imfilter(imfilter(imfilter(dxI.*dtI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdtx = wdtx(:,:,:,2);
+
+wdty = imfilter(imfilter(imfilter(dyI.*dtI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdty = wdty(:,:,:,2);
+
+wdtz = imfilter(imfilter(imfilter(dzI.*dtI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdtz = wdtz(:,:,:,2);   
+
+clear dtI
+
+% Spatial Components
+wdx2 = imfilter(imfilter(imfilter(dxI.*dxI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdx2 = wdx2(:,:,:,2);
+
+wdy2 = imfilter(imfilter(imfilter(dyI.*dyI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdy2 = wdy2(:,:,:,2);
+
+wdz2 = imfilter(imfilter(imfilter(dzI.*dzI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdz2 = wdz2(:,:,:,2);
+
+wdxy = imfilter(imfilter(imfilter(dxI.*dyI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdxy = wdxy(:,:,:,2);
+
+wdxz = imfilter(imfilter(imfilter(dxI.*dzI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdxz = wdxz(:,:,:,2);
+
+clear dxI
+
+wdyz = imfilter(imfilter(imfilter(dzI.*dyI, xFil5, 'replicate'), yFil5, 'replicate'), zFil5,'replicate');
+wdyz = wdyz(:,:,:,2);
+
+clear dyI dzI
+clear xFil5 yFil5 zFil5
+
+
+%% Optical Flow calculations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Equation is v = (A' w A)^-1 A' w b
+% A = -[dxI dyI dzI]
+% b = [dtI]
+% w multiplication is incorporated in the structure tensor inputs above
+% A' w b = -[wdtx wdty wdtz]  (minus sign because of negative sign on A)
+% (A' w A) = [a=wdx2 b=wdxy c=wdxz ; d=wdxy e=wdy2 f=wdyz ; g=wdxz h=wdyz i=wdz2]
+% (A' w A)^-1 = 1/determinant * [A D G ; B E H ; C F I];
+
+% Determinant
+determinant = (wdx2.*wdy2.*wdz2) + (2*wdxy.*wdxz.*wdyz) - (wdy2.*wdxz.^2) - (wdz2.*wdxy.^2) - (wdx2.*wdyz.^2);
+
+% using inverse notation from here: https://en.wikipedia.org/wiki/Invertible_matrix#Inversion_of_3_%C3%97_3_matrices
+%     A = wdy2.*wdz2 - wdyz.*wdyz; %ei-fh
+%     B = wdyz.*wdxz - wdxy.*wdz2; %fg-di
+%     C = wdxy.*wdyz - wdy2.*wdxz; %dh-eg
+%     D = wdxz.*wdyz - wdxy.*wdz2; %ch-bi
+%     E = wdx2.*wdz2 - wdxz.*wdxz; %ai-cg
+%     F = wdxy.*wdxz - wdx2.*wdyz; %bg-ah
+%     G = wdxy.*wdyz - wdxz.*wdy2; %bf-ce
+%     H = wdxz.*wdxy - wdx2.*wdyz; %cd-af
+%     I = wdx2.*wdy2 - wdxy.*wdxy; %ae-bd
+% 
+%     vx = ((determinant + eps).^-1).*(A.*wdtx + D.*wdty + G.*wdtz);
+%     vy = ((determinant + eps).^-1).*(B.*wdtx + E.*wdty + H.*wdtz);
+%     vz = ((determinant + eps).^-1).*(C.*wdtx + F.*wdty + I.*wdtz);
+
+% Velocity calculations
+vx = -((determinant + eps).^-1).*((wdy2.*wdz2 - wdyz.*wdyz).*wdtx + (wdxz.*wdyz - wdxy.*wdz2).*wdty + (wdxy.*wdyz - wdxz.*wdy2).*wdtz);
+vy = -((determinant + eps).^-1).*((wdyz.*wdxz - wdxy.*wdz2).*wdtx + (wdx2.*wdz2 - wdxz.*wdxz).*wdty + (wdxz.*wdxy - wdx2.*wdyz).*wdtz);
+vz = -((determinant + eps).^-1).*((wdxy.*wdyz - wdy2.*wdxz).*wdtx + (wdxy.*wdxz - wdx2.*wdyz).*wdty + (wdx2.*wdy2 - wdxy.*wdxy).*wdtz);
+
+clear determinant
+
+%% Eigenvalues for Reliability %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% solve det(A^T w A - lamda I) = 0
+% (A' w A) = [a=wdx2 b=wdxy c=wdxz ; d=wdxy e=wdy2 f=wdyz ; g=wdxz h=wdyz i=wdz2]
+% det(A^T w A - lambda I) = (a-lambda)(e-lambda)(i-lambda) + 2*b*c*f - c^2(e-lambda) - b^2(i-lamda) - f^2(a-lambda)
+
+% Solutions to the polynomial equation are from Wolfram Alpha
+
+L1 = (2*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3*wdy2.*wdx2.^2 + sqrt(4*(-wdx2.^2 + wdx2.*wdz2 + wdy2.*wdx2 - 3.*wdxy.^2 - ...
+    3*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + wdy2.*wdz2 - wdy2.^2).^3 + (2*wdx2.^3 - 3*wdx2.^2.*wdz2 - ...
+    3.*wdy2.*wdx2.^2 + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + ...
+    12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + ...
+    9*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9*wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - ...
+    3.*wdy2.^2.*wdz2 + 2*wdy2.^3).^2) + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - ...
+    3*wdx2.*wdz2.^2 + 12.*wdy2.*wdx2.*wdz2 - 3.*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + ...
+    54.*wdxy.*wdxz.*wdyz + 9.*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9.*wdy2.*wdyz.^2 + ...
+    2.*wdz2.^3 - 3.*wdy2.*wdz2.^2 - 3.*wdy2.^2.*wdz2 + 2.*wdy2.^3).^(1/3)./(3*2^(1/3))....
+    - (2^(1/3)*(-wdx2.^2 + wdx2.*wdz2 + wdy2.*wdx2 - 3.*wdxy.^2 - 3*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + ...
+    wdy2.*wdz2 - wdy2.^2))./(3*(2*wdx2.^3 - 3.*wdx2.^2.*wdz2 - 3.*wdy2.*wdx2.^2 + ...
+    sqrt(4*(-wdx2.^2 + wdx2.*wdz2 + wdy2.*wdx2 - 3.*wdxy.^2 - 3*wdxz.^2 - 3.*wdyz.^2 - wdz2.^2 + ...
+    wdy2.*wdz2 - wdy2.^2).^3 + (2.*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3.*wdy2.*wdx2.^2 + 9*wdx2.*wdxy.^2 + ...
+    9.*wdx2.*wdxz.^2 - 18.*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12.*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 ...
+    - 18*wdxy.^2.*wdz2 + 9.*wdy2.*wdxy.^2 + 54.*wdxy.*wdxz.*wdyz + 9.*wdxz.^2.*wdz2 - 18.*wdy2.*wdxz.^2 +...
+    9*wdyz.^2.*wdz2 + 9.*wdy2.*wdyz.^2 + 2.*wdz2.^3 - 3*wdy2.*wdz2.^2 - 3*wdy2.^2.*wdz2 + 2*wdy2.^3).^2) ...
+    + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - ...
+    3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + 54.*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - ...
+    18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9*wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - 3*wdy2.^2.*wdz2 + ...
+    2*wdy2.^3).^(1/3)) + (1/3)*(wdx2 + wdz2 + wdy2);
+
+L2 = -((1 - wdz2.*sqrt(3)).*(2.*wdx2.^3 - 3.*wdx2.^2.*wdz2 - 3.*wdy2.*wdx2.^2 + ...
+    sqrt(4*(-wdx2.^2 + wdx2.*wdz2 + wdy2.*wdx2 - 3.*wdxy.^2 - 3*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + ...
+    wdy2.*wdz2 - wdy2.^2).^3 + (2.*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3.*wdy2.*wdx2.^2 + 9*wdx2.*wdxy.^2 + ...
+    9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - ...
+    18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + ...
+    9*wdyz.^2.*wdz2 +  wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - 3.*wdy2.^2.*wdz2 + 2*wdy2.^3).^2)...
+    + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - ...
+    3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - ...
+    18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9*wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - ...
+    3*wdy2.^2.*wdz2 + 2*wdy2.^3).^(1/3))./(6*2^(1/3)) + ((1 + wdz2*sqrt(3)).*(-wdx2.^2 + ...
+    wdx2.*wdz2 + wdy2.*wdx2 - 3*wdxy.^2 - 3.*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + wdy2.*wdz2 - wdy2.^2))./...
+    (3*2^(2/3)*(2*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3*wdy2.*wdx2.^2 + sqrt(4*(-wdx2.^2 + wdx2.*wdz2 + ...
+    wdy2.*wdx2 - 3*wdxy.^2 - 3*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + wdy2.*wdz2 - wdy2.^2).^3 + ...
+    (2*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3*wdy2.*wdx2.^2 + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - ...
+    3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + ...
+    54*wdxy.*wdxz.*wdyz + 9.*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9*wdy2.*wdyz.^2 + 2*wdz2.^3 ...
+    - 3*wdy2.*wdz2.^2 - 3*wdy2.^2.*wdz2 + 2*wdy2.^3).^2) + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - ...
+    18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + ...
+    9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + ...
+    9*wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - 3*wdy2.^2.*wdz2 + 2*wdy2.^3).^(1/3)) + 1/3*(wdx2 + wdz2 + wdy2);
+
+rel = min(L1,L2);
+clear L1 L2
+
+L3 = -((1 + wdz2.*sqrt(3)).*(2.*wdx2.^3 - 3.*wdx2.^2.*wdz2 - 3.*wdy2.*wdx2.^2 + ...
+    sqrt(4*(-wdx2.^2 + wdx2.*wdz2 + wdy2.*wdx2 - 3.*wdxy.^2 - 3*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + ...
+    wdy2.*wdz2 - wdy2.^2).^3 + (2.*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3.*wdy2.*wdx2.^2 + 9*wdx2.*wdxy.^2 + ...
+    9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - ...
+    18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + ...
+    9*wdyz.^2.*wdz2 +  wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - 3.*wdy2.^2.*wdz2 + 2*wdy2.^3).^2)...
+    + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - ...
+    3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - ...
+    18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9*wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - ...
+    3*wdy2.^2.*wdz2 + 2*wdy2.^3).^(1/3))./(6*2^(1/3)) + ((1 - wdz2*sqrt(3)).*(-wdx2.^2 + ...
+    wdx2.*wdz2 + wdy2.*wdx2 - 3*wdxy.^2 - 3.*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + wdy2.*wdz2 - wdy2.^2))./...
+    (3*2^(2/3)*(2*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3*wdy2.*wdx2.^2 + sqrt(4*(-wdx2.^2 + wdx2.*wdz2 + ...
+    wdy2.*wdx2 - 3*wdxy.^2 - 3*wdxz.^2 - 3*wdyz.^2 - wdz2.^2 + wdy2.*wdz2 - wdy2.^2).^3 + ...
+    (2*wdx2.^3 - 3*wdx2.^2.*wdz2 - 3*wdy2.*wdx2.^2 + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - 18*wdx2.*wdyz.^2 - ...
+    3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + 9*wdy2.*wdxy.^2 + ...
+    54*wdxy.*wdxz.*wdyz + 9.*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + 9*wdy2.*wdyz.^2 + 2*wdz2.^3 ...
+    - 3*wdy2.*wdz2.^2 - 3*wdy2.^2.*wdz2 + 2*wdy2.^3).^2) + 9*wdx2.*wdxy.^2 + 9*wdx2.*wdxz.^2 - ...
+    18*wdx2.*wdyz.^2 - 3*wdx2.*wdz2.^2 + 12*wdy2.*wdx2.*wdz2 - 3*wdy2.^2.*wdx2 - 18*wdxy.^2.*wdz2 + ...
+    9*wdy2.*wdxy.^2 + 54*wdxy.*wdxz.*wdyz + 9*wdxz.^2.*wdz2 - 18*wdy2.*wdxz.^2 + 9*wdyz.^2.*wdz2 + ...
+    9*wdy2.*wdyz.^2 + 2*wdz2.^3 - 3*wdy2.*wdz2.^2 - 3*wdy2.^2.*wdz2 + 2*wdy2.^3).^(1/3)) + 1/3*(wdx2 + wdz2 + wdy2);
+
+rel = real(min(rel,L3));
+clear L3
+
+
