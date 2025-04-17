3db5db5063bd408d0489917c05dbf3b16802e5bf
Rachel Lee <65371136+ScientistRachel@users.noreply.github.com>
2025-04-17 11:22:53 -0400

    Comment I/O functions

--- src/MATLAB/TIFFvolume.m
+++ src/MATLAB/TIFFvolume.m
@@ -1,6 +1,21 @@
-% Use TIFF to load in 3D volumes
 function vol = TIFFvolume(filename,Nx,Ny,frames)
-
+% Use TIFF to load in 3D volumes
+%
+% TIFF files are read using the TIFF library.
+%
+% vol = TIFFvolume(filename,Nx,Ny,frames)
+%
+% INPUTS:
+% filename  = path for the saved file
+% Nx        = Size of the image in X (width)
+% Ny        = Size of the image in Y (height)
+% frames    = Number of frames from the volume to load. This can be time
+%               and/or z depending on the input file.
+%
+% OUTPUTS:
+% vol       = Matrix of size [Ny,Nx,frames] and type double
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     vol = ones(Ny,Nx,frames);
     intiff = Tiff(filename,"r");
     for ii = 1:frames-1
